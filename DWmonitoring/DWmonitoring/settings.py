ede48198955db584db60cb9da95ce20e6da8a0f7
shyam <shyam.bista3210@gmail.com>
2024-05-26 13:16:10 +0545

    added views

--- DWmonitoring/DWmonitoring/settings.py
+++ DWmonitoring/DWmonitoring/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
+
+AUTHENTICATION_BACKENDS = [
+    'authentication.backends.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
--- DWmonitoring/authentication/urls.py
+++ DWmonitoring/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView, HomeView
 
 urlpatterns = [
+    path('/',HomeView.as_view(), name="home"),
+    path('/login',LoginView.as_view(), name="login"),
     path('/register',RegisterView.as_view(), name="register"),
 ]
