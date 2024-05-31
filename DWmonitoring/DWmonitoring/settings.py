ab700198a20f595e7513d63d77f5f4ca6e4aaf35
sudeep-gtn <sudeep.bogati@greentick.com.np>
2024-05-31 13:08:36 +0545

    register and login page final

--- DWmonitoring/DWmonitoring/settings.py
+++ DWmonitoring/DWmonitoring/settings.py
@@ -133,3 +133,22 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 AUTH_USER_MODEL = 'authentication.CustomUser'
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
--- DWmonitoring/authentication/views.py
+++ DWmonitoring/authentication/views.py
@@ -3,19 +3,36 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import CustomUser
 from django.contrib.auth import authenticate,login,logout
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.contrib.auth.password_validation import validate_password
+import re
 
 class RegisterView(View):
     def get(self, request):
         return render(request, "register.html")
 
     def post(self, request):
-        full_name = request.POST.get("full_name")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        c_password = request.POST.get("c_password")
+        full_name = request.POST.get("full_name").strip()
+        email = request.POST.get("email").strip()
+        password = request.POST.get("password").strip()
+        c_password = request.POST.get("c_password").strip()
+
+        if not re.match(r'^[A-Za-z\s]{3,}$', full_name):
+            return render(request, "register.html", {"error": "Full name must be at least 3 characters long and contain only alphabetic characters and spaces"})
 
         if password != c_password:
             return render(request, "register.html", {"error": "Passwords do not match"})
+        try:
+            validate_email(email)
+        except ValidationError:
+            return render(request, "register.html", {"error": "Invalid email address"})
+
+        # Validate password
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            return render(request, "register.html", {"error": str(e.messages)})
 
         try:
             user = CustomUser.objects.create_user(
