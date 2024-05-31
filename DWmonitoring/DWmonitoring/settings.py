c6ff95c69568612f7ad241cfdba4414aadc78b4c
sudeep-gtn <sudeep.bogati@greentick.com.np>
2024-05-31 12:02:36 +0545

    Login page first commit

--- DWmonitoring/DWmonitoring/settings.py
+++ DWmonitoring/DWmonitoring/settings.py
@@ -116,21 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+from pathlib import Path
 STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/main/",
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
+    
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'authentication.CustomUser'
-
-AUTHENTICATION_BACKENDS = [
-    'authentication.backends.EmailBackend',
-    'django.contrib.auth.backends.ModelBackend',
-]
