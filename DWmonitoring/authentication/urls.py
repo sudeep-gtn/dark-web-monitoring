f01520fce28105bdfe1036430dabecdd00fe4b30
shyam-gtn <shyam.bista@greentick.com.np>
2024-06-21 15:35:48 +0545

    styling

--- DWmonitoring/authentication/urls.py
+++ DWmonitoring/authentication/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path
 from .views import (
-    RegisterView, LoginView, 
-    HomeView, LogoutView, 
-    ProfileView, 
-   TermsAndConditionsView,
-    BrandProtectionView,EditNameView,
+    RegisterView, LoginView,
+    HomeView, LogoutView,
+    ProfileView,
+    TermsAndConditionsView,
+    BrandProtectionView, EditNameView,
     ChangePasswordView, ContactPageView,
-    VerifyOTP,SendOTPFromProfile
+    VerifyOTP, SendOTPFromProfile
     )
 
 urlpatterns = [
-    path('',HomeView.as_view(), name="home"),
-    path('login',LoginView.as_view(), name="login"),
-    path('register',RegisterView.as_view(), name="register"),
-    path('logout',LogoutView.as_view(), name="logout"),
-    path('profile',ProfileView.as_view(), name="profile"),
-    path('terms-and-conditions', TermsAndConditionsView.as_view(), name="terms-and-conditions"),
-    path('brand-protection',BrandProtectionView.as_view(), name="brand-protection"),
+    path('', HomeView.as_view(), name="home"),
+    path('login', LoginView.as_view(), name="login"),
+    path('register', RegisterView.as_view(), name="register"),
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('profile', ProfileView.as_view(), name="profile"),
+    path('terms-and-conditions', TermsAndConditionsView.as_view(), name='''terms-
+         and-conditions'''),
+    path('brand-protection', BrandProtectionView.as_view(), name="brand-protection"),
     path('edit-name', EditNameView.as_view(), name="edit-name"),
     path('change-password', ChangePasswordView.as_view(), name="change-password"),
     path('contact', ContactPageView.as_view(), name="contact"),
